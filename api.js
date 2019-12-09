3850ec255cbc2df511e78d48ed5e71f8391833e6
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2019-12-09 18:00:48 +0200

    Add files via upload
    post updated

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
+
 // get a ninja in db
 router.get("/ninjas",function(req,res){
   res.send({type:"GET"});
@@ -9,16 +10,19 @@ router.get("/ninjas",function(req,res){
 
 // adda ninja to db
 router.post("/ninjas",function(req,res){
-  res.send({type:"POST"});
+  //console.log(req.body);   // can use this because of bodyParser
+  res.send({type:"POST",
+            name:req.body.name,
+            rank:req.body.rank});
 });
 
 // update a ninja in db
-router.put("/ninjas:id",function(req,res){
+router.put("/ninjas/:id",function(req,res){
   res.send({type:"PUT"});
 });
 
 // delete a ninja from the db
-router.delete("/ninjas:id",function(req,res){
+router.delete("/ninjas/:id",function(req,res){
   res.send({type:"DELETE"});
 });
 
