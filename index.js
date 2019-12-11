2e17319512763fea5c9f96fd43370d03b1d3256d
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2019-12-11 04:42:46 +0200

    Add files via upload
    connected to db

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const routes = require("./routes/api"); // so that we can access routes
+
+const mongoose = require("mongoose");
 // set up express app
+const app  = express();  // comes with http methods
 
-const app = express();
-
-const routes = require("./routes/api");
-
-app.use(bodyParser.json);//so we can use data
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');//will create ninjago database
 
 
-app.use("/api",routes); // will use all the routes we specified
-// listen for requests
+mongoose.Promise = global.Promise;
+
+
+app.use(bodyParser.json());//will pass json data and attatch it to request
+
+//initialise routes
+app.use('/api',routes); //uses routes -- so that you didnt have to put api down each time
+
+
+
+//listen for request
 app.listen(4000,function(){
-  console.log("Now Lstening to port 4000");
+  console.log("now listening to port 4000");
 });
